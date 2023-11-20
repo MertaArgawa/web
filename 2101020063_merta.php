8b9f20d1b4e9d4228d41b5b501269adb50397752
MertaArgawa <151506053+MertaArgawa@users.noreply.github.com>
2023-11-20 15:30:30 +0800

    Add files via upload

---
+++ 2101020063_merta.php
@@ -0,0 +1,12 @@
+<?php
+
+    $server     = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "2101020063_merta";
+
+    $con = mysqli_connect($server,$user,$password,$database)
+
+?>
+
+
